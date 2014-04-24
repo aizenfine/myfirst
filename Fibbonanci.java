7f034650c099311dbf8a9fddeeda20dab7d16fae
Faishal Izzan Nahidha <yugiholic@gmail.com>
2014-04-24 17:26:13 +0700

    Update Fibbonanci.java
    commit 2

--- Fibbonanci.java
+++ Fibbonanci.java
@@ -16,12 +16,15 @@ public class Fibbonanci {
         n=s.nextInt();
         int a=0;
         int b=1;
-        System.out.print(n+" deret Fibonacci: " );
+        int c=0;
+        //System.out.print(n+" deret Fibonacci: " );
         for (int i=1;i<=n;i++){
-            System.out.print(a+" ");
+            //System.out.print(a+" ");
             a=a+b;
-            b=a-b;   
+            b=a-b;
+            c=c+a;
         }
+        System.out.print("jumlah Fibonacci: "+c);
     }
         
 }
